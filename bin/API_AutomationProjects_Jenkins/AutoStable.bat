fa1447027e48797b452d45b0932f4ba8af67e855
Divyarani <dvrani.163@gmail.com>
2018-02-14 12:19:24 +0530

    Done

--- bin/API_AutomationProjects_Jenkins/AutoStable.bat
+++ bin/API_AutomationProjects_Jenkins/AutoStable.bat
@@ -1,3 +1,5 @@
 set current_dir=%cd%
 echo %current_dir%
-C:\Jenkins_WorkSpace\ReadyAPI-2.2.0\bin\testrunner.bat -f%current_dir%\API_Automation_Reports -FPDF -R"Project Report" "-EAuto" -I "%current_dir%\API_AutomationProjects_ReadyAPI\AutoStable.xml"
+testrunner.bat -f%current_dir%\API_Automation_Reports -FPDF -R"Project Report" "-EAuto" -I "%current_dir%\API_AutomationProjects_ReadyAPI\AutoStable.xml"
+
+pause
