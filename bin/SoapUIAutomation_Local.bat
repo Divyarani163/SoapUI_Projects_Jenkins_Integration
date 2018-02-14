d65bac6abf7a6efde1108c8ce942988a9f1856a0
Divyarani <dvrani.163@gmail.com>
2018-02-14 13:15:01 +0530

    deleted

--- bin/SoapUIAutomation_Local.bat
+++
@@ -1,5 +0,0 @@
-set current_dir=%cd%
-set xml=%1
-echo %xml%
-echo %current_dir%
-%current_dir%testrunner.bat -f%current_dir%\API_AutomationProjects_Jenkins\API_Automation_Reports -FPDF -R"Project Report" "-EAuto" -I "%current_dir%\API_AutomationProjects_Jenkins\API_AutomationProjects_ReadyAPI\%xml%"
