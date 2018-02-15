a280f86be28ac5794e4f29df627a33f6df744dd9
Divyarani <dvrani.163@gmail.com>
2018-02-15 12:00:40 +0530

    Done modified

--- bin/SoapUIAutomation_Jenkins.bat
+++ bin/SoapUIAutomation_Jenkins.bat
@@ -2,4 +2,4 @@ set current_dir=%cd%
 set xml=%1
 echo %xml%
 echo %current_dir%
-%current_dir%\bin\testrunner.bat -r -a -f%current_dir%\bin\API_Automation_Reports -FPDF -R"Project Report" "-EAuto" -I "%current_dir%\bin\API_AutomationProjects_ReadyAPI\%xml%"
+%current_dir%\bin\testrunner.bat -f%current_dir%\bin\API_Automation_Reports -FPDF -R"Project Report" "-EAuto" -I "%current_dir%\bin\API_AutomationProjects_ReadyAPI\%xml%"
