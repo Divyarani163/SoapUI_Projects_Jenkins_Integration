dcdb768aa410f81725870fef7280ec1e50952ca3
Divyarani <dvrani.163@gmail.com>
2018-02-16 14:52:33 +0530

    Modified

--- bin/SoapUIAutomation_Jenkins.bat
+++ bin/SoapUIAutomation_Jenkins.bat
@@ -6,6 +6,6 @@ echo %xml%
 echo %current_dir%
 set current_date_time=%DATE%
 echo %DATE%
-%current_dir%\bin\testrunner.bat -f%current_dir%\bin\API_Automation_Reports -FPDF -R"Project Report"%DATE% "-E%var%" -I "%current_dir%\bin\API_AutomationProjects_ReadyAPI\%xml%"
+%current_dir%\bin\testrunner.bat -f%current_dir%\bin\API_Automation_Reports -FPDF -R"Project Report" "-E%var%" -I "%current_dir%\bin\API_AutomationProjects_ReadyAPI\%xml%"
 
 
