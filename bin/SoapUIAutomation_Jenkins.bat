b2f84bab72843365a495346f1c802fd92205af6d
Divyarani <dvrani.163@gmail.com>
2018-02-16 13:18:28 +0530

    done

--- bin/SoapUIAutomation_Jenkins.bat
+++ bin/SoapUIAutomation_Jenkins.bat
@@ -4,8 +4,8 @@ set var=%2
 echo %var%
 echo %xml%
 echo %current_dir%
-set current_date_time="`date +%Y%m%d%H%M%S`";
-echo $current_date_time;
-%current_dir%\bin\testrunner.bat -f%current_dir%\bin\API_Automation_Reports -FPDF -R"Project Report $current_date_time" "-E%var%" -I "%current_dir%\bin\API_AutomationProjects_ReadyAPI\%xml%"
+set current_date_time=%DATE%
+echo %DATE%
+%current_dir%\bin\testrunner.bat -f%current_dir%\bin\API_Automation_Reports -FPDF -R"Project Report%DATE%" "-E%var%" -I "%current_dir%\bin\API_AutomationProjects_ReadyAPI\%xml%"
 
 
