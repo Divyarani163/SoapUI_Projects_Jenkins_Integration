41163333d5ba8c151e4ba465909d48ea46845461
Divyarani Rapolu <divyarani.rapolu@infor.com>
2018-03-16 10:34:15 +0530

    Removed and Added

--- bin/SoapUIAutomation_Jenkins.bat
+++
@@ -1,9 +0,0 @@
-set current_dir=%cd%
-set xml=%1
-set var=%2
-echo %var%
-echo %xml%
-echo %current_dir%
-%current_dir%\bin\testrunner.bat -r -a -f%cd%\API_Automation_Reports -FPDF -R"Project Report" "-E%var%" -I "%current_dir%\bin\API_AutomationProjects_ReadyAPI\%xml%"
-
-
