e6648250c5ab606b15866bb6fa151535b8d7dc17
Divyarani Rapolu <divyarani.rapolu@infor.com>
2018-03-16 10:43:11 +0530

    done

--- bin/AutoStagePRD_Projects.bat
+++ bin/AutoStagePRD_Projects.bat
@@ -27,6 +27,7 @@ set var9=%9
 echo %var7%
 echo %var8%
 echo %var9%
-%current_dir%\bin\testrunner.bat -r -a -f%cd%\API_Automation_Reports -FPDF -R"Project Report" -PEndpoint=%1 -PTenantId=%2 -PToken=%3 -PToken_URL=%4 -PClient_ID=%5 -PClient_Secret=%6 -PServiceAccountAccessKey_SAAK=%7 -PServiceAccountSecretKey_SASK=%8 -PURL_SCIM=%9 -PUsername_SCIM=%7 -PPwd_SCIM=%8 -PDomain_SCIM=%9 -I "%current_dir%\bin\API_AutomationProjects_ReadyAPI\AutoStagePRD.xml"
+%current_dir%\bin\testrunner.bat -r -a -f%cd%\API_Automation_Reports -FPDF -R"Project Report" -PEndpoint=%1 -PTenantId=%2 -PToken=%3 -PToken_URL=%4 -PClient_ID=%5 -PClient_Secret=%6 -PServiceAccountAccessKey_SAAK=%7 -PServiceAccountSecretKey_SASK=%8 -PURL_SCIM=%9 -PUsername_SCIM=%7 -PPwd_SCIM=%8 -PDomain_SCIM=%9 -I 
+"%current_dir%\bin\API_AutomationProjects_ReadyAPI\AutoStagePRD.xml"
 
 
