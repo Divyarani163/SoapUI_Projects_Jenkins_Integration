7425e7e7a9c56fb46eb205a63975eaa9805560d0
Divyarani Rapolu <divyarani.rapolu@infor.com>
2018-03-19 17:21:35 +0530

    done

--- bin/AutoStagePRD_Projects.bat
+++ bin/AutoStagePRD_Projects.bat
@@ -33,8 +33,7 @@ set Domain_SCIM=%3
 echo %Username_SCIM%
 echo %Pwd_SCIM%
 echo %Domain_SCIM%
-%current_dir%\bin\testrunner.bat -r -a -f%cd%\API_Automation_Reports -FPDF -R"Project Report" -PEndpoint=%1 -PTenantId=%2 -PToken=%3 -PToken_URL=%4 -PClient_ID=%5 -PClient_Secret=%6 -PServiceAccountAccessKey_SAAK=%7 -PServiceAccountSecretKey_SASK=%8 -PURL_SCIM=%9 -PUsername_SCIM=%1 -PPwd_SCIM=%2 -PDomain_SCIM=%3 -I "%current_dir%\bin\API_AutomationProjects_ReadyAPI\AutoStagePRD.xml"
+%current_dir%\bin\testrunner.bat -r -a -f%cd%\API_Automation_Reports -FPDF -R"Project Report" -PEndpoint=%Endpoint% -PTenantId=%TenantId% -PToken=%Token% -PToken_URL=%Token_URL% -PClient_ID=%Client_ID% -PClient_Secret=%Client_Secret% -PServiceAccountAccessKey_SAAK=%ServiceAccountAccessKey_SAAK% -PServiceAccountSecretKey_SASK=%ServiceAccountSecretKey_SASK% -PURL_SCIM=%URL_SCIM% -PUsername_SCIM=%Username_SCIM% -PPwd_SCIM=%Pwd_SCIM% -PDomain_SCIM=%Domain_SCIM% -I "%current_dir%\bin\API_AutomationProjects_ReadyAPI\AutoStagePRD.xml"
 
-pause
 
 
