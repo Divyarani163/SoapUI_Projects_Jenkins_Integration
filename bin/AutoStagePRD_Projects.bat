fb49302e0fc9481a8a4fb09761a6532d00e42b9c
Divyarani Rapolu <divyarani.rapolu@infor.com>
2018-03-19 17:04:24 +0530

    modified again

--- bin/AutoStagePRD_Projects.bat
+++ bin/AutoStagePRD_Projects.bat
@@ -1,32 +1,40 @@
 set current_dir=%cd%
-set var1=%1
-set var2=%2
-set var3=%3
-set var4=%4
-set var5=%5
-set var6=%6
-set var7=%7
-set var8=%8
-set var9=%9
-echo %var1%
-echo %var2%
-echo %var3%
-echo %var4%
-echo %var5%
-echo %var6%
-echo %var7%
-echo %var8%
-echo %var9%
+set Endpoint=%1
+set TenantId=%2
+set Token=%3
+set Token_URL=%4
+set Client_ID=%5
+set Client_Secret=%6
+set ServiceAccountAccessKey_SAAK=%7
+set ServiceAccountSecretKey_SASK=%8
+set URL_SCIM=%9
+echo %Endpoint%
+echo %TenantId%
+echo %Token%
+echo %Token_URL%
+echo %Client_ID%
+echo %Client_Secret%
+echo %ServiceAccountAccessKey_SAAK%
+echo %ServiceAccountSecretKey_SASK%
+echo %URL_SCIM%
 echo %current_dir%
 shift
 shift
 shift
-set var7=%7
-set var8=%8
-set var9=%9
-echo %var7%
-echo %var8%
-echo %var9%
-%current_dir%\bin\testrunner.bat -r -a -f%cd%\API_Automation_Reports -FPDF -R"Project Report" -PEndpoint=%1 -PTenantId=%2 -PToken=%3 -PToken_URL=%4 -PClient_ID=%5 -PClient_Secret=%6 -PServiceAccountAccessKey_SAAK=%7 -PServiceAccountSecretKey_SASK=%8 -PURL_SCIM=%9 -PUsername_SCIM=%7 -PPwd_SCIM=%8 -PDomain_SCIM=%9 -I "%current_dir%\bin\API_AutomationProjects_ReadyAPI\AutoStagePRD.xml"
+shift
+shift
+shift
+shift
+shift
+shift
+set Username_SCIM=%1
+set Pwd_SCIM=%2
+set Domain_SCIM=%3
+echo %Username_SCIM%
+echo %Pwd_SCIM%
+echo %Domain_SCIM%
+%current_dir%\bin\testrunner.bat -r -a -f%cd%\API_Automation_Reports -FPDF -R"Project Report" -PEndpoint=%1 -PTenantId=%2 -PToken=%3 -PToken_URL=%4 -PClient_ID=%5 -PClient_Secret=%6 -PServiceAccountAccessKey_SAAK=%7 -PServiceAccountSecretKey_SASK=%8 -PURL_SCIM=%9 -PUsername_SCIM=%1 -PPwd_SCIM=%2 -PDomain_SCIM=%3 -I "%current_dir%\bin\API_AutomationProjects_ReadyAPI\AutoStagePRD.xml"
+
+pause
 
 
